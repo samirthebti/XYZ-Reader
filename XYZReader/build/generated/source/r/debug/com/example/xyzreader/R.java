f3127eeff46b1193a4894d81466f00032dccaf01
samirthebti <thebtisam@gmail.com>
2016-11-11 04:47:53 +0100

    feat : add RLT support

--- XYZReader/build/generated/source/r/debug/com/example/xyzreader/R.java
+++ XYZReader/build/generated/source/r/debug/com/example/xyzreader/R.java
@@ -3049,6 +3049,7 @@ containing a value of this type.
         public static final int bottom_sheet_behavior=0x7f060025;
         public static final int character_counter_pattern=0x7f060026;
         public static final int errer_message=0x7f060027;
+        public static final int refreshed_string=0x7f060028;
         public static final int search_menu_title=0x7f060013;
         public static final int status_bar_notification_info_overflow=0x7f060014;
     }
